c3b6f28edaa2f26af9087c3ea911046ae3d75efd
Jin <j@shopar.dev>
2023-08-05 01:29:00 +0700

    change: remove get authorization header method

--- src/Webhook.php
+++ src/Webhook.php
@@ -74,7 +74,15 @@ class Webhook
 
     public function verify($signature = null, $rawData = null, $throw = true)
     {
-        $signature = $signature ?? self::getAuthorizationHeader();
+        $signature = $signature ?? $_SERVER['HTTP_AUTHORIZATION'] ?? null;
+        if ($signature === null) {
+            if ($throw) {
+                throw new TiktokShopException("Incoming webhook request invalid. No authorization header.");
+            }
+
+            return false;
+        }
+
         $rawData = $rawData ?? file_get_contents('php://input');
 
         $stringToBeSigned = $this->client->getAppKey() . $rawData;
@@ -87,24 +95,5 @@ class Webhook
         }
 
         return $verified;
-    }
-
-    private static function getAuthorizationHeader()
-    {
-        $headers = null;
-        if (isset($_SERVER['Authorization'])) {
-            $headers = trim($_SERVER["Authorization"]);
-        } elseif (isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
-        } elseif (function_exists('apache_request_headers')) {
-            $requestHeaders = apache_request_headers();
-            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-
-            if (isset($requestHeaders['Authorization'])) {
-                $headers = trim($requestHeaders['Authorization']);
-            }
-        }
-
-        return $headers;
     }
 }
