d72d3cb4cc28aa3fb4acbc682d0d94e3d04eef99
Jin <j@shopar.dev>
2023-10-27 02:51:47 +0700

    change: product api

--- src/Resources/Product.php
+++ src/Resources/Product.php
@@ -11,84 +11,176 @@
 namespace NVuln\TiktokShop\Resources;
 
 use GuzzleHttp\RequestOptions;
-use NVuln\TiktokShop\Resource;
 use SplFileInfo;
 
-class Product extends Resource
+/**
+ * separate global product api to easier managed
+ */
+class Product extends GlobalProduct
 {
-    public function uploadFile($file, $file_name = 'uploaded_file')
+    protected $category = 'product';
+
+    public function uploadProductFile($file, $file_name = 'uploaded_file.pdf')
     {
         if ($file instanceof SplFileInfo) {
             $file_name = $file->getFilename();
         }
 
-        return $this->call('POST', 'products/upload_files', [
+        return $this->call('POST', 'files/upload', [
             RequestOptions::JSON => [
-                'file_name' => $file_name,
-                'file_data' => static::dataTypeCast('file', $file),
+                'name' => $file_name,
+                'data' => static::dataTypeCast('file', $file),
             ]
         ]);
     }
 
-    public function uploadImage($image, $scene = 1 /* PRODUCT_IMAGE */)
+    public function uploadProductImage($image, $use_case = 'MAIN_IMAGE')
     {
-        return $this->call('POST', 'products/upload_imgs', [
+        return $this->call('POST', 'images/upload', [
             RequestOptions::JSON => [
-                'img_data' => static::dataTypeCast('image', $image),
-                'img_scene' => $scene,
+                'data' => static::dataTypeCast('image', $image),
+                'use_case' => $use_case,
             ]
         ]);
     }
 
-    public function createProduct($data = [])
+    public function createProduct($data)
     {
         return $this->call('POST', 'products', [
             RequestOptions::JSON => $data
         ]);
     }
 
-    public function createDraftProduct($data = [])
-    {
-        return $this->call('POST', 'products/save_draft', [
-            RequestOptions::JSON => $data
-        ]);
-    }
-
-    public function deleteProduct($product_ids = [])
+    public function deleteProducts($product_ids = [])
     {
         return $this->call('DELETE', 'products', [
             RequestOptions::JSON => [
-                'product_ids' => static::dataTypeCast('array', $product_ids)
+                'product_ids' => $product_ids,
             ]
         ]);
     }
 
     public function editProduct($product_id, $data = [])
     {
-        $data['product_id'] = $product_id;
-
-        return $this->call('PUT', 'products', [
+        return $this->call('PUT', 'products/'.$product_id, [
             RequestOptions::JSON => $data
         ]);
     }
 
-    public function updateStock($product_id, $skus = [])
+    public function updateInventory($product_id, $params = [])
     {
-        $data = [
-            'product_id' => $product_id,
-            'skus' => $skus
-        ];
-
-        return $this->call('PUT', 'products/stocks', [
-            RequestOptions::JSON => $data
+        return $this->call('POST', 'products/'.$product_id.'/inventory/update', [
+            RequestOptions::JSON => $params
         ]);
     }
 
-    public function getProductList($params = [])
+    public function getProduct($product_id, $params = [])
+    {
+        return $this->call('GET', 'products/'.$product_id, [
+            RequestOptions::QUERY => $params,
+        ]);
+    }
+
+    public function deactivateProducts($product_ids = [])
+    {
+        return $this->call('POST', 'products/deactivate', [
+            RequestOptions::JSON => [
+                'product_ids' => $product_ids,
+            ]
+        ]);
+    }
+
+    public function activateProducts($product_ids = [])
+    {
+        return $this->call('POST', 'products/activate', [
+            RequestOptions::JSON => [
+                'product_ids' => $product_ids,
+            ]
+        ]);
+    }
+
+    public function recoverProducts($product_ids = [])
+    {
+        return $this->call('POST', 'products/recover', [
+            RequestOptions::JSON => [
+                'product_ids' => $product_ids,
+            ]
+        ]);
+    }
+
+    public function updatePrice($product_id, $params)
+    {
+        return $this->call('POST', 'products/'.$product_id.'/prices/update', [
+            RequestOptions::JSON => $params
+        ]);
+    }
+
+    public function getCategories($params = [])
+    {
+        return $this->call('GET', 'categories', [
+            RequestOptions::QUERY => $params
+        ]);
+    }
+
+    public function getBrands($params = [])
     {
         $params = array_merge([
-            'page_number' => 1,
             'page_size' => 20,
+        ], $params);
+
+        return $this->call('GET', 'brands', [
+            RequestOptions::QUERY => $params
+        ]);
+    }
+
+    public function createCustomBrand($name)
+    {
+        return $this->call('POST', 'brands', [
+            RequestOptions::JSON => [
+                'name' => $name
+            ]
+        ]);
+    }
+
+    public function getAttributes($category_id, $params = [])
+    {
+        return $this->call('GET', 'categories/'.$category_id.'/attributes', [
+            RequestOptions::QUERY => $params
+        ]);
+    }
+
+    public function getCategoryRules($category_id)
+    {
+        return $this->call('GET', 'categories/'.$category_id.'/rules');
+    }
+
+    public function recommendCategory($product_title, $description = '', $images = [])
+    {
+        return $this->call('POST', 'categories/recommend', [
+            RequestOptions::JSON => [
+                'product_title' => $product_title,
+                'description' => $description,
+                'images' => $images,
+            ]
+        ]);
+    }
+
+    public function checkListingPrerequisites()
+    {
+        return $this->call('GET', 'prerequisites');
+    }
+
+    public function partialEditProduct($product_id, $params = [])
+    {
+        return $this->call('POST', 'products/'.$product_id.'/partial_edit', [
+            RequestOptions::JSON => $params
+        ]);
+    }
+
+    public function searchProducts($params = [])
+    {
+        $params = array_merge([
+            'page_size' => 20, // required
         ], $params);
 
         return $this->call('POST', 'products/search', [
@@ -96,124 +188,10 @@ class Product extends Resource
         ]);
     }
 
-    public function getProductDetail($product_id, $need_audit_version = false)
+    public function inventorySearch($params = [])
     {
-        return $this->call('GET', 'products/details', [
-            RequestOptions::QUERY => [
-                'product_id' => $product_id,
-                'need_audit_version' => static::dataTypeCast('bool', $need_audit_version),
-            ],
-        ]);
-    }
-
-    public function deactivateProduct($product_ids = [])
-    {
-        return $this->call('POST', 'products/inactivated_products', [
-            RequestOptions::JSON => [
-                'product_ids' => static::dataTypeCast('array', $product_ids),
-            ]
-        ]);
-    }
-
-    public function activateProduct($product_ids = [])
-    {
-        return $this->call('POST', 'products/activate', [
-            RequestOptions::JSON => [
-                'product_ids' => static::dataTypeCast('array', $product_ids),
-            ]
-        ]);
-    }
-
-    public function recoverDeletedProduct($product_ids = [])
-    {
-        return $this->call('POST', 'products/recover', [
-            RequestOptions::JSON => [
-                'product_ids' => static::dataTypeCast('array', $product_ids),
-            ]
-        ]);
-    }
-
-    public function updatePrice($product_id, $skus)
-    {
-        $data = [
-            'product_id' => $product_id,
-            'skus' => $skus
-        ];
-
-        return $this->call('PUT', 'products/prices', [
-            RequestOptions::JSON => $data
-        ]);
-    }
-
-    public function getCategories()
-    {
-        return $this->call('GET', 'products/categories');
-    }
-
-    public function getBrands(array $params)
-    {
-        return $this->call('GET', 'products/brands', [
-            RequestOptions::QUERY => $params
-        ]);
-    }
-
-    public function createBrand(string $brand_name)
-    {
-        return $this->call('POST', 'products/brand', [
-            RequestOptions::JSON => [
-                'brand_name' => $brand_name
-            ]
-        ]);
-    }
-
-    public function getAttributes($category_id)
-    {
-        return $this->call('GET', 'products/attributes', [
-            RequestOptions::QUERY => [
-                'category_id' => static::dataTypeCast('string', $category_id),
-            ]
-        ]);
-    }
-
-    public function getCategoryRule($category_id)
-    {
-        return $this->call('GET', 'products/categories/rules', [
-            RequestOptions::QUERY => [
-                'category_id' => $category_id
-            ]
-        ]);
-    }
-
-    public function getProductStock($product_ids = [], $sku_ids = [])
-    {
-        return $this->call('POST', 'products/stock/list', [
-            RequestOptions::QUERY => [
-                'version' => '202305',
-            ],
-            RequestOptions::JSON => [
-                'product_ids' => static::dataTypeCast('array', $product_ids),
-                'sku_ids' => static::dataTypeCast('array', $sku_ids),
-            ]
-        ]);
-    }
-
-    public function categoryRecommended(string $product_name, string $description = '', array $images = [])
-    {
-        return $this->call('POST', 'product/category_recommend', [
-            RequestOptions::JSON => [
-                'product_name' => $product_name,
-                'description' => $description,
-                'images' => $images,
-            ]
-        ]);
-    }
-
-    public function precheckForOperatingProduct()
-    {
-        return $this->call('POST', 'product/pre_check', [
-            RequestOptions::QUERY => [
-                'version' => '202306',
-            ]
+        return $this->call('POST', 'inventory/search', [
+            RequestOptions::JSON => $params
         ]);
     }
 }
--- tests/Resources/GlobalProductTest.php
+++ tests/Resources/GlobalProductTest.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of tiktokshop-client.
  *
- * (c) Jin <j@sax.vn>
+ * Copyright (c) 2023 Jin <j@sax.vn> All rights reserved.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -10,62 +10,79 @@
 
 namespace NVuln\TiktokShop\Tests\Resources;
 
+use NVuln\TiktokShop\Resources\GlobalProduct;
 use NVuln\TiktokShop\Tests\TestResource;
+use PHPUnit\Framework\TestCase;
 
+/**
+ * @property-read \NVuln\TiktokShop\Resources\GlobalProduct $caller
+ */
 class GlobalProductTest extends TestResource
 {
 
     public function testGetGlobalCategories()
     {
         $this->caller->getGlobalCategories();
-        $this->assertPreviousRequest('GET', 'product/global_products/categories');
-    }
-
-    public function testEditGlobalProduct()
-    {
-        $this->caller->editGlobalProduct(1, []);
-        $this->assertPreviousRequest('PUT', 'product/global_products');
-    }
-
-    public function testGetGlobalProductList()
-    {
-        $this->caller->getGlobalProductList();
-        $this->assertPreviousRequest('POST', 'product/global_products/search');
-    }
-
-    public function testPublishGlobalProduct()
-    {
-        $this->caller->publishGlobalProduct(1, []);
-        $this->assertPreviousRequest('POST', 'product/global_products/publish');
+        $this->assertPreviousRequest('GET', 'product/'.TestResource::TEST_API_VERSION.'/global_categories');
     }
 
     public function testGetGlobalAttributes()
     {
-        $this->caller->getGlobalAttributes(1);
-        $this->assertPreviousRequest('GET', 'product/global_products/attributes');
+        $this->caller->getGlobalAttributes(1009);
+        $this->assertPreviousRequest('GET', 'product/'.TestResource::TEST_API_VERSION.'/categories/1009/global_attributes');
     }
 
-    public function testGetGlobalCategoryRule()
+    public function testGetGlobalCategoryRules()
     {
-        $this->caller->getGlobalCategoryRule(1);
-        $this->assertPreviousRequest('GET', 'product/global_products/categories/rules');
+        $this->caller->getGlobalCategoryRules(1009);
+        $this->assertPreviousRequest('GET', 'product/'.TestResource::TEST_API_VERSION.'/categories/1009/global_rules');
     }
 
-    public function testGetGlobalProductDetail()
+    public function testUpdateGlobalInventory()
     {
-        $this->caller->getGlobalProductDetail(1);
-        $this->assertPreviousRequest('GET', 'product/global_products');
+        $this->caller->updateGlobalInventory(1009, []);
+        $this->assertPreviousRequest('POST', 'product/'.TestResource::TEST_API_VERSION.'/global_products/1009/inventory/update');
+    }
+
+    public function testEditGlobalProduct()
+    {
+        $this->caller->editGlobalProduct(1009, []);
+        $this->assertPreviousRequest('PUT', 'product/'.TestResource::TEST_API_VERSION.'/global_products/1009');
+    }
+
+    public function testSearchGlobalProducts()
+    {
+        $this->caller->searchGlobalProducts();
+        $this->assertPreviousRequest('POST', 'product/'.TestResource::TEST_API_VERSION.'/global_products/search');
+    }
+
+    public function testGetGlobalProduct()
+    {
+        $this->caller->getGlobalProduct(1009);
+        $this->assertPreviousRequest('GET', 'product/'.TestResource::TEST_API_VERSION.'/global_products/1009');
+    }
+
+    public function testDeleteGlobalProducts()
+    {
+        $this->caller->deleteGlobalProducts([]);
+        $this->assertPreviousRequest('DELETE', 'product/'.TestResource::TEST_API_VERSION.'/global_products');
     }
 
     public function testCreateGlobalProduct()
     {
-        $this->caller->createGlobalProduct(1, []);
-        $this->assertPreviousRequest('POST', 'product/global_products');
+        $this->caller->createGlobalProduct([]);
+        $this->assertPreviousRequest('POST', 'product/'.TestResource::TEST_API_VERSION.'/global_products');
     }
 
-    public function testUpdateGlobalProductPrice()
+    public function testRecommendGlobalCategories()
     {
-        $this->caller->updateGlobalProductPrice(1, []);
-        $this->assertPreviousRequest('PUT', 'product/global_products/prices');
+        $this->caller->recommendGlobalCategories([]);
+        $this->assertPreviousRequest('POST', 'product/'.TestResource::TEST_API_VERSION.'/global_categories/recommend');
+    }
+
+    public function testPublishGlobalProduct()
+    {
+        $this->caller->publishGlobalProduct(1009, []);
+        $this->assertPreviousRequest('POST', 'product/'.TestResource::TEST_API_VERSION.'/global_products/1009/publish');
     }
 }
