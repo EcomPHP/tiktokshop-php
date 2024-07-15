1f971fbcf975af5c284db40eff35f79adb05a59a
nVuln <j@shopar.dev>
2024-07-15 20:39:12 +0700

    update: affiliate seller api

--- src/Resources/AffiliateSeller.php
+++ src/Resources/AffiliateSeller.php
@@ -16,11 +16,80 @@ use GuzzleHttp\RequestOptions;
 class AffiliateSeller extends Resource
 {
     protected $category = 'affiliate_seller';
-    protected $minimum_version = 202405;
+    protected $minimum_version = 202406;
 
     public function editOpenCollaborationSettings($body)
     {
         return $this->call('POST', 'open_collaboration_settings', [
+            RequestOptions::JSON => $body,
+        ]);
+    }
+
+    public function searchOpenCollaborationProduct($query = [], $body = [])
+    {
+        $query = array_merge([
+            'page_size' => 10,
+        ], $query);
+
+        return $this->call('POST', 'open_collaborations/products/search', [
+            RequestOptions::QUERY => $query,
+            RequestOptions::JSON => $body,
+        ]);
+    }
+
+    public function searchSellerAffiliateOrders($query = [])
+    {
+        $query = array_merge([
+            'page_size' => 10,
+        ], $query);
+
+        return $this->call('POST', 'orders/search', [
+            RequestOptions::QUERY => $query,
+            RequestOptions::JSON => [],
+        ]);
+    }
+
+    public function createOpenCollaboration($product_id, $commission_rate, $require_seller_approve_creator = false)
+    {
+        return $this->call('POST', 'open_collaborations', [
+            RequestOptions::JSON => [
+                'product_id' => $product_id,
+                'commission_rate' => $commission_rate,
+                'require_seller_approve_creator' => $require_seller_approve_creator,
+            ],
+        ]);
+    }
+
+    public function createTargetCollaboration($body)
+    {
+        return $this->call('POST', 'target_collaborations', [
+            RequestOptions::JSON => $body,
+        ]);
+    }
+
+    public function removeCreatorAffiliateFromCollaboration($open_collaboration_id, $creator_user_id, $product_id)
+    {
+        return $this->call('POST', 'open_collaborations/'.$open_collaboration_id.'/remove_creator', [
+            RequestOptions::JSON => [
+                'creator_user_id' => $creator_user_id,
+                'product_id' => $product_id,
+            ],
+        ]);
+    }
+
+    public function getMarketplaceCreatorPerformance($creator_user_id)
+    {
+        return $this->call('GET', 'marketplace_creators/'.$creator_user_id);
+    }
+
+    public function searchCreatorOnMarketplace($query = [], $body = [])
+    {
+        $query = array_merge([
+            'page_size' => 12,
+        ], $query);
+
+        return $this->call('POST', 'marketplace_creators/search', [
+            RequestOptions::QUERY => $query,
             RequestOptions::JSON => $body,
         ]);
     }
--- tests/Resources/AffiliateSellerTest.php
+++ tests/Resources/AffiliateSellerTest.php
@@ -12,9 +12,12 @@ namespace EcomPHP\TiktokShop\Tests\Resources;
 
 use EcomPHP\TiktokShop\Tests\TestResource;
 
+/**
+ * @property-read \EcomPHP\TiktokShop\Resources\AffiliateSeller $caller
+ */
 class AffiliateSellerTest extends TestResource
 {
-    public const TEST_API_VERSION = 202405;
+    public const TEST_API_VERSION = 202406;
 
     protected function tiktokShopClientForTest()
     {
@@ -28,5 +31,47 @@ class AffiliateSellerTest extends TestResource
     {
         $this->caller->editOpenCollaborationSettings([]);
         $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/open_collaboration_settings');
+    }
+
+    public function testSearchOpenCollaborationProduct()
+    {
+        $this->caller->searchOpenCollaborationProduct();
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/open_collaborations/products/search');
+    }
+
+    public function testSearchSellerAffiliateOrders()
+    {
+        $this->caller->searchSellerAffiliateOrders();
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/orders/search');
+    }
+
+    public function testCreateOpenCollaboration()
+    {
+        $this->caller->createOpenCollaboration(1, 1);
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/open_collaborations');
+    }
+
+    public function testCreateTargetCollaboration()
+    {
+        $this->caller->createTargetCollaboration([]);
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/target_collaborations');
+    }
+
+    public function testRemoveCreatorAffiliateFromCollaboration()
+    {
+        $this->caller->removeCreatorAffiliateFromCollaboration(1, 2, 3);
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/open_collaborations/1/remove_creator');
+    }
+
+    public function testGetMarketplaceCreatorPerformance()
+    {
+        $this->caller->getMarketplaceCreatorPerformance(1);
+        $this->assertPreviousRequest('GET', 'affiliate_seller/'.self::TEST_API_VERSION.'/marketplace_creators/1');
+    }
+
+    public function testSearchCreatorOnMarketplace()
+    {
+        $this->caller->searchCreatorOnMarketplace();
+        $this->assertPreviousRequest('POST', 'affiliate_seller/'.self::TEST_API_VERSION.'/marketplace_creators/search');
     }
 }
